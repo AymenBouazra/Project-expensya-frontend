07cf1bcdc53b3bdcbf58d6a806c10e86953365a0
Aymen <aymenbouazra994@gmail.com>
2021-06-24 21:36:14 +0100

    installed path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express= require('express');
 const path = require('path');
 const app =express();
 
-app.use(express.static(__dirname+'/dist/expensya-frontend'));
+app.use(express.static(__dirname+'/dist/Expensya-frontend'));
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist/expensya-frontend/index.html'))
+    res.sendFile(path.join(__dirname+'/dist/Expensya-frontend/index.html'))
 })
 
 app.listen(process.env.PORT || 8080);
